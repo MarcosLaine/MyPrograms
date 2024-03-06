6e0298e3e8ff0a9c0d0d4183839791c2cf9b1d20
Marcos Paulo Laine <marcospslaine@gmail.com>
2024-03-06 16:33:09 -0300

    atualizações

--- TPs/TP1/CifrementoRec.java
+++ TPs/TP1/CifrementoRec.java
@@ -18,17 +18,20 @@
 } */
 
 public class CifrementoRec {
+
     static String cifrar(String str, int chave, int i){
-        if(i == str.length())
+        if(i >= str.length()) // Se i for maior ou igual ao comprimento da string, terminamos a recursão.
             return "";
         else
-            return cifrar(str,chave,i+1)+(char)(str.charAt(i)  + chave);
+            // Primeiro fazemos a chamada recursiva para o próximo caractere e depois adicionamos o caractere cifrado.
+            return (char)(str.charAt(i) + chave) + cifrar(str, chave, i + 1);
     }
     
     public static void main(String[] args) {
         String entrada = MyIO.readLine();
-        while( !entrada.equalsIgnoreCase("FIM") ){
-            entrada = cifrar(entrada, 3, entrada.length()-1);
+        while(!entrada.equalsIgnoreCase("FIM")){
+            // A chamada para a cifragem começa com i = 0.
+            entrada = cifrar(entrada, 3, 0);
             MyIO.println(entrada);
             entrada = MyIO.readLine();
         }
