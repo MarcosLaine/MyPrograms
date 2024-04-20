f2bf659aa8fb17b0f425a0c91ea7ca5ef39ae67d
Marcos Laine <marcospslaine@gmail.com>
2024-04-20 20:40:48 -0300

    Update paths and references from Windows to Linux system in a C# console project
    The changes include:
    - Updating the project file path, packages path, output path, and runtime identifier graph path from Windows to Linux format
    - Updating

--- C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6044e880a6c60974ce37cb842576e0255769c7ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910c10699f3b1abcea5b00c6420298c06f3e1a6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
