c831a57216e8772a1c2863182bd87268de6271ea
Marcos Laine <marcospslaine@gmail.com>
2024-04-29 11:33:05 -0300

    linux commit

--- C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910c10699f3b1abcea5b00c6420298c06f3e1a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c916150c83ab8628e9e325a94df4b03f8f90e68")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
