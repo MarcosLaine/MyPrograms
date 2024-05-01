7a0d4b7ba56a0172a533be40270bb67e8921f87f
Marcos Paulo Laine <marcospslaine@gmail.com>
2024-05-01 20:38:20 -0300

    AssemblyInfo informational version changed, and binary differences in Palindromo.class and palindromo.class.
    The AssemblyInfo file in the Debug/net8.0 folder of the ConsoleApp1 project has an updated Assembly

--- C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86c8d10aa53a607b1cc2267bf2e4554e134e831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ff44d159719fcb0d69da3a06d461f04f1452e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
